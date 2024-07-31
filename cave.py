a1d599493cfa3720ccdd5f1cfd6082f18369dcc7
Michael Brooks <michael.brooks@det.nsw.edu.au>
2024-07-31 04:33:53 +0000

    Created the cave.py and main.py files

---
+++ cave.py
@@ -0,0 +1,4 @@
+class Cave:
+    def __init__(self, cave_name):
+        self.name = cave_name
+        self.description = None
---
+++ main.py
@@ -0,0 +1,5 @@
+from cave import Cave
+
+cavern = Cave("Cavern")
+
+print(cavern)
