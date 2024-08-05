9c6ae65de3e47b1d810e37fce1da9442643a7976
Michael Brooks <michael.brooks@det.nsw.edu.au>
2024-08-05 14:54:32 +1000

    Can now set linked caves and recall details

--- main.py
+++ main.py
@@ -3,3 +3,17 @@ from cave import Cave
 cavern = Cave("Cavern")
 grotto = Cave("Grotto")
 dungeon = Cave("Dungeon")
+
+cavern.set_description("A damp and dirty cave.")
+grotto.set_description("A small cave with ancient markings.")
+dungeon.set_description("A large cave with a rack.")
+
+cavern.link_cave("south", dungeon)
+dungeon.link_cave("north", cavern)
+dungeon.link_cave("west", grotto)
+grotto.link_cave("east", dungeon)
+
+dungeon.get_details()
+cavern.get_details()
+grotto.get_details()
+                       
